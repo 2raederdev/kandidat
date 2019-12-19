80de8d917eb2bafe14a245f2fd6adadfdf4cf586
Daniel <2raederdev@gmail.com>
2019-12-19 17:13:03 +0100

    Deploy 2.0

---
+++ server/public/precache-manifest.0d31c4532ec276a131d58a49f8d09c71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33aef6a7243674cbf538c93d3644a168",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e08d5ed69dc35b16f167",
+    "url": "/static/css/2.3785f652.chunk.css"
+  },
+  {
+    "revision": "57c3c55adf3e0634616f",
+    "url": "/static/css/main.af751bcb.chunk.css"
+  },
+  {
+    "revision": "e08d5ed69dc35b16f167",
+    "url": "/static/js/2.b60609c5.chunk.js"
+  },
+  {
+    "revision": "1e9eb255d40c233d7b4bcbf4b89ec654",
+    "url": "/static/js/2.b60609c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57c3c55adf3e0634616f",
+    "url": "/static/js/main.86255190.chunk.js"
+  },
+  {
+    "revision": "16bf403f85a67a0fb936",
+    "url": "/static/js/runtime-main.12a6431d.js"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.22cd7cd1352066640e6164b4f52b8103.js"
+  "/precache-manifest.0d31c4532ec276a131d58a49f8d09c71.js"
 );
 
 self.addEventListener('message', (event) => {
